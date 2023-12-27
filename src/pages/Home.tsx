3e00deffc98f095c37b94db6251f5400a605d94a
HaNNa89 <petersson.h@hotmail.com>
2023-12-27 14:22:53 +0100

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 
 function Home() {
 	return (
 		<Flex>
 			<Heading>Home!</Heading>
+			<Text>Hej home!</Text>
 		</Flex>
 	);
 }
