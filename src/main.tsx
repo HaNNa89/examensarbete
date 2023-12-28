589089e89025376c729a93417fdabd895ebbfec9
HaNNa89 <petersson.h@hotmail.com>
2023-12-28 22:36:51 +0100

    import and install 2 fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,55 +1,56 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "@fontsource/imprima";
+import "@fontsource/indie-flower";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom';
-import App from './App.tsx';
-import Admin from './pages/Admin.tsx';
-import Checkout from './pages/Checkout.tsx';
-import Detail from './pages/Detail.tsx';
-import Favorites from './pages/Favorites.tsx';
-import Home from './pages/Home.tsx';
-import OrderConfirmation from './pages/OrderConfirmation.tsx';
-import SearchResult from './pages/SearchResult.tsx';
-
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from "react-router-dom";
+import App from "./App.tsx";
+import Admin from "./pages/Admin.tsx";
+import Checkout from "./pages/Checkout.tsx";
+import Detail from "./pages/Detail.tsx";
+import Favorites from "./pages/Favorites.tsx";
+import Home from "./pages/Home.tsx";
+import OrderConfirmation from "./pages/OrderConfirmation.tsx";
+import SearchResult from "./pages/SearchResult.tsx";
 const theme = extendTheme({
-  styles: {
-    global: {
-      body: {
-        backgroundColor: '#1A1A1C',
-        color: 'white',
-        margin: 0,
-      },
-    },
-  },
-  fonts: {
-    heading: `'Indie Flower', sans-serif`,
-    body: `'Imprima', sans-serif`,
-  },
+	styles: {
+		global: {
+			body: {
+				backgroundColor: "#1A1A1C",
+				color: "white",
+				margin: 0,
+			},
+		},
+	},
+	fonts: {
+		heading: `'Indie Flower', sans-serif`,
+		body: `'Imprima', sans-serif`,
+	},
 });
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-      <Route path="checkout" element={<Checkout />} />
-      <Route path="favorites" element={<Favorites />} />
-      <Route path="/id/:productName" element={<Detail />} />
-      <Route path="admin" element={<Admin />} />
-      <Route path="orderconfirmation" element={<OrderConfirmation />} />
-      <Route path="search-results/:term" element={<SearchResult />} />
-    </Route>
-  )
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route index element={<Home />} />
+			<Route path="checkout" element={<Checkout />} />
+			<Route path="favorites" element={<Favorites />} />
+			<Route path="/id/:productName" element={<Detail />} />
+			<Route path="admin" element={<Admin />} />
+			<Route path="orderconfirmation" element={<OrderConfirmation />} />
+			<Route path="search-results/:term" element={<SearchResult />} />
+		</Route>
+	)
 );
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<ChakraProvider theme={theme}>
+			<RouterProvider router={router} />
+		</ChakraProvider>
+	</React.StrictMode>
 );
