183f54b9d839402954c996289638e43e7fc4dadb
Tara skoglund <skoglundtara@gmail.com>
2023-12-28 16:37:00 +0100

    add productCard to Home

--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,55 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
-} from "react-router-dom";
-import App from "./App.tsx";
-import Admin from "./pages/Admin.tsx";
-import Checkout from "./pages/Checkout.tsx";
-import Detail from "./pages/Detail.tsx";
-import Favorites from "./pages/Favorites.tsx";
-import Home from "./pages/Home.tsx";
-import OrderConfirmation from "./pages/OrderConfirmation.tsx";
-import SearchResult from "./pages/SearchResult.tsx";
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import App from './App.tsx';
+import Admin from './pages/Admin.tsx';
+import Checkout from './pages/Checkout.tsx';
+import Detail from './pages/Detail.tsx';
+import Favorites from './pages/Favorites.tsx';
+import Home from './pages/Home.tsx';
+import OrderConfirmation from './pages/OrderConfirmation.tsx';
+import SearchResult from './pages/SearchResult.tsx';
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: '#1A1A1C',
+        color: 'white',
+        margin: 0,
+      },
+    },
+  },
+  fonts: {
+    heading: `'Indie Flower', sans-serif`,
+    body: `'Imprima', sans-serif`,
+  },
+});
 
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<App />}>
-			<Route index element={<Home />} />
-			<Route path="checkout" element={<Checkout />} />
-			<Route path="favorites" element={<Favorites />} />
-			<Route path="/id/:productName" element={<Detail />} />
-			<Route path="admin" element={<Admin />} />
-			<Route path="orderconfirmation" element={<OrderConfirmation />} />
-			<Route path="search-results/:term" element={<SearchResult />} />
-		</Route>
-	)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="checkout" element={<Checkout />} />
+      <Route path="favorites" element={<Favorites />} />
+      <Route path="/id/:productName" element={<Detail />} />
+      <Route path="admin" element={<Admin />} />
+      <Route path="orderconfirmation" element={<OrderConfirmation />} />
+      <Route path="search-results/:term" element={<SearchResult />} />
+    </Route>
+  )
 );
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<ChakraProvider>
-			<RouterProvider router={router} />
-		</ChakraProvider>
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,31 @@
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Center, Grid } from '@chakra-ui/react';
+import { MOCK_PRODUCTS } from '../../data/mock';
+import ProductCard from '../components/ProductCard';
 
 function Home() {
-	return (
-		<Flex>
-			<Heading>Home!</Heading>
-			<Text>Hej home!</Text>
-		</Flex>
-	);
+  return (
+    <Box mx="auto" maxW="1200px" px="1rem">
+      <Center>
+        <Box w="100%" mt="2rem">
+          <Center>
+            <Grid
+              templateColumns={{
+                base: 'repeat(1, 1fr)',
+                md: 'repeat(2, 1fr)',
+                lg: 'repeat(4, 1fr)',
+              }}
+              gap={6}
+              justifyContent="center" // Centrerar innehållet horisontellt
+            >
+              {MOCK_PRODUCTS.map((product, index) => (
+                <ProductCard key={index} product={product} />
+              ))}
+            </Grid>
+          </Center>
+        </Box>
+      </Center>
+    </Box>
+  );
 }
 
 export default Home;
