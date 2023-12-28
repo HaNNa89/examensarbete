5098fd0f8bf42c478125b826b46e6c5309b563c6
HaNNa89 <petersson.h@hotmail.com>
2023-12-28 23:03:18 +0100

    change icons and add spacing + padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,25 +7,31 @@ import {
 	InputRightElement,
 } from "@chakra-ui/react";
 import { FaSearch } from "react-icons/fa";
-import { FaHeart, FaUser } from "react-icons/fa6";
-import { PiHandbagSimpleFill } from "react-icons/pi";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { PiUser } from "react-icons/pi";
 
 function Header() {
 	return (
-		<Flex justify="space-between" align="center">
+		<Flex px={6} py={2} justify="space-between" align="center">
 			<Heading>Café o´neil</Heading>
 			<Flex position="relative" alignSelf="center">
 				<InputGroup>
 					<Input placeholder="search" w="300px" />
 					<InputRightElement>
-						<IconButton icon={<FaSearch />} aria-label="search" fontSize={25} />
+						<IconButton
+							icon={<FaSearch />}
+							aria-label="search"
+							size="xs"
+							fontSize={20}
+						/>
 					</InputRightElement>
 				</InputGroup>
 			</Flex>
-			<Flex gap={2} fontSize={25}>
-				<FaHeart />
-				<PiHandbagSimpleFill />
-				<FaUser />
+			<Flex gap={4} fontSize={30}>
+				<PiUser />
+				<IoMdHeartEmpty />
+				<HiOutlineShoppingBag />
 			</Flex>
 		</Flex>
 	);
