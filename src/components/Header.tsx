65bf88e4d283d9e36f285b00e32939f319a405cb
HaNNa89 <petersson.h@hotmail.com>
2023-12-28 21:53:29 +0100

    add header with icon, search bar + logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,32 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import {
+	Flex,
+	Heading,
+	IconButton,
+	Input,
+	InputGroup,
+	InputRightElement,
+} from "@chakra-ui/react";
+import { FaSearch } from "react-icons/fa";
+import { FaHeart, FaUser } from "react-icons/fa6";
+import { PiHandbagSimpleFill } from "react-icons/pi";
 
 function Header() {
 	return (
-		<Flex>
-			<Heading>Header!</Heading>
+		<Flex justify="space-between" align="center">
+			<Heading>Café o´neil</Heading>
+			<Flex position="relative" alignSelf="center">
+				<InputGroup>
+					<Input placeholder="search" w="300px" />
+					<InputRightElement>
+						<IconButton icon={<FaSearch />} aria-label="search" fontSize={25} />
+					</InputRightElement>
+				</InputGroup>
+			</Flex>
+			<Flex gap={2} fontSize={25}>
+				<FaHeart />
+				<PiHandbagSimpleFill />
+				<FaUser />
+			</Flex>
 		</Flex>
 	);
 }
