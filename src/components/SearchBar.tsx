3b9c52854037c2fd3a1bf23c79a73f40d2fe3fb4
HaNNa89 <petersson.h@hotmail.com>
2024-01-02 23:47:50 +0100

    rendering search result + styling

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,34 +1,44 @@
 import {
 	Flex,
 	IconButton,
+	Image,
 	Input,
 	InputGroup,
 	InputRightElement,
+	Text,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { MOCK_PRODUCTS } from "../../data/mock";
 
 interface ProductItems {
 	title: string;
+	categorie: string;
+	img: string;
 }
+
 function SearchBar() {
 	const [searchProduct, setSearchproduct] = useState("");
 	const [searchResult, setSearchResult] = useState<ProductItems[]>([]);
 
-	const handleSearch = () => {
-		const filteredProductItems = MOCK_PRODUCTS.filter((product) =>
-			product.title.toLowerCase().includes(searchProduct.toLowerCase())
-		);
-		setSearchResult(filteredProductItems);
-		console.log(filteredProductItems);
-	};
+	useEffect(() => {
+		if (searchProduct.trim() === "") {
+			setSearchResult([]);
+		} else {
+			const filteredProductItems = MOCK_PRODUCTS.filter(
+				(product: ProductItems) =>
+					product.title.toLowerCase().includes(searchProduct.toLowerCase())
+			);
+			setSearchResult(filteredProductItems);
+		}
+	}, [searchProduct]);
+
 	return (
-		<Flex position="relative" alignSelf="center">
+		<Flex position="relative">
 			<InputGroup>
 				<Input
 					placeholder="search"
-					w="300px"
+					maxW="300px"
 					onChange={(event) => {
 						setSearchproduct(event.target.value);
 					}}
@@ -40,10 +50,44 @@ function SearchBar() {
 						aria-label="search"
 						size="xs"
 						fontSize={20}
-						onClick={handleSearch}
 					/>
 				</InputRightElement>
 			</InputGroup>
+
+			<Flex
+				direction="column"
+				justifyContent="flex-start"
+				position="absolute"
+				zIndex={1}
+				top="100%"
+				left={0}
+				bg="#1A1A1C"
+				p={2}
+			>
+				<Text>Result: </Text>
+				{searchResult.length === 0 ? (
+					<Text>No result found</Text>
+				) : (
+					<Flex direction="column">
+						{searchResult.map((result, index) => (
+							<Flex
+								align="center"
+								justifyContent="space-between"
+								p={2}
+								key={index}
+							>
+								<Text>{result.title}</Text>
+								<Image
+									src={result.img}
+									maxW="30%"
+									maxH="30%"
+									objectFit="cover"
+								/>
+							</Flex>
+						))}
+					</Flex>
+				)}
+			</Flex>
 		</Flex>
 	);
 }
