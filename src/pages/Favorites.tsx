520a02417302cf773d4b1e8f6018d5c4fbf20e41
Tara skoglund <skoglundtara@gmail.com>
2024-01-03 15:14:29 +0100

    add favorites to page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,11 +1,39 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Grid, Heading } from '@chakra-ui/react';
+import ProductCard from '../components/ProductCard';
+
+interface FavoriteProduct {
+  img: string;
+  title: string;
+  price: number;
+}
 
 function Favorites() {
-	return (
-		<Flex>
-			<Heading>Favorites!</Heading>
-		</Flex>
-	);
+  const favoriteProductsFromLocalStorage: FavoriteProduct[] = JSON.parse(
+    localStorage.getItem('favoriteProducts') || '[]'
+  );
+
+  return (
+    <Flex direction="column" alignItems="center">
+      <Heading m="8">Favorites!</Heading>
+      <Flex flexWrap="wrap" justifyContent="center">
+        <Grid
+          templateColumns={{
+            base: 'repeat(1, 1fr)',
+            md: 'repeat(2, 1fr)',
+            lg: 'repeat(4, 1fr)',
+          }}
+          gap={6}
+          justifyContent="center"
+        >
+          {favoriteProductsFromLocalStorage.map(
+            (product: FavoriteProduct, index) => (
+              <ProductCard key={index} product={product} />
+            )
+          )}
+        </Grid>
+      </Flex>
+    </Flex>
+  );
 }
 
 export default Favorites;
