2cdaf88391109527aba25a11b5d582680430963c
HaNNa89 <petersson.h@hotmail.com>
2024-01-12 00:29:37 +0100

    Fix problem in ProductCard -add "subheading" to  Product interface. And removed the link to page

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -22,6 +22,7 @@ interface Product {
 	title: string;
 	price: number;
 	id: number;
+	subheading: string;
 }
 
 interface CartContext {
@@ -57,22 +58,22 @@ function ProductCard({ product }: { product: Product }) {
 
 	return (
 		<Flex>
-			<Link to={`/id/${product.title}`}>
-				{isLargeScreen ? (
-					<Box
-						width="15rem"
-						height="20rem"
-						boxShadow="0 4px 8px 0 rgba(0, 0, 0, 0.2)"
-						borderRadius="10px"
-						overflow="hidden"
-						bg="white"
-						position="relative"
-						transition="transform 0.3s"
-						_hover={{ transform: "scale(1.08)" }}
-						cursor="pointer"
-						onMouseEnter={() => setShowOverlay(true)}
-						onMouseLeave={() => setShowOverlay(false)}
-					>
+			{isLargeScreen ? (
+				<Box
+					width="15rem"
+					height="20rem"
+					boxShadow="0 4px 8px 0 rgba(0, 0, 0, 0.2)"
+					borderRadius="10px"
+					overflow="hidden"
+					bg="white"
+					position="relative"
+					transition="transform 0.3s"
+					_hover={{ transform: "scale(1.08)" }}
+					cursor="pointer"
+					onMouseEnter={() => setShowOverlay(true)}
+					onMouseLeave={() => setShowOverlay(false)}
+				>
+					<Link to={`/id/${product.title}`}>
 						<Image
 							src={img}
 							alt={title}
@@ -80,73 +81,21 @@ function ProductCard({ product }: { product: Product }) {
 							width="100%"
 							height="100%"
 						/>
-						<Box
-							bg="rgba(0, 0, 0, 0.6)"
-							p="2px"
-							color="white"
-							position="absolute"
-							top="60%"
-							left="0"
-							right="0"
-							bottom="0"
-							opacity={showOverlay ? "1" : "0"}
-							transition="opacity 0.3s"
-							zIndex="1"
-						>
-							<Flex direction="column">
-								<Flex justify="space-between" align="center">
-									<Box>
-										<Heading fontSize={20}>{title}</Heading>
-										<Text fontSize={16} mt="0.2rem">
-											Price: {price}kr
-										</Text>
-									</Box>
-									<IconButton
-										icon={isProductLiked ? <GoHeartFill /> : <GoHeart />}
-										aria-label={isProductLiked ? "Unlike" : "Like"}
-										fontSize="1.5rem"
-										w="fit-content"
-										bg="none"
-										color="white"
-										_hover={{ bg: "none" }}
-										onClick={handleToggleLike}
-									/>
-								</Flex>
-								<Button
-									bg="none"
-									border="1px"
-									mt="0.2rem"
-									borderColor="white"
-									textColor="white"
-									_hover={{
-										bg: "whiteAlpha.200",
-										borderWidth: "2px",
-										boxShadow: "0 4px 8px rgba(255, 255, 255, 0.3)",
-									}}
-									onClick={handleOrderClick}
-								>
-									Order
-								</Button>
-							</Flex>
-						</Box>
-					</Box>
-				) : (
-					<Flex direction="column" width="15rem">
-						<Box width="15rem" height="20rem">
-							<Image
-								src={img}
-								alt={title}
-								objectFit="cover"
-								width="100%"
-								height="100%"
-							/>
-						</Box>
-						<Flex
-							direction="column"
-							height="8rem"
-							m="1"
-							justify="space-between"
-						>
+					</Link>
+					<Box
+						bg="rgba(0, 0, 0, 0.6)"
+						p="2px"
+						color="white"
+						position="absolute"
+						top="60%"
+						left="0"
+						right="0"
+						bottom="0"
+						opacity={showOverlay ? "1" : "0"}
+						transition="opacity 0.3s"
+						zIndex="1"
+					>
+						<Flex direction="column">
 							<Flex justify="space-between" align="center">
 								<Box>
 									<Heading fontSize={20}>{title}</Heading>
@@ -181,9 +130,56 @@ function ProductCard({ product }: { product: Product }) {
 								Order
 							</Button>
 						</Flex>
+					</Box>
+				</Box>
+			) : (
+				<Flex direction="column" width="15rem">
+					<Box width="15rem" height="20rem">
+						<Image
+							src={img}
+							alt={title}
+							objectFit="cover"
+							width="100%"
+							height="100%"
+						/>
+					</Box>
+					<Flex direction="column" height="8rem" m="1" justify="space-between">
+						<Flex justify="space-between" align="center">
+							<Box>
+								<Heading fontSize={20}>{title}</Heading>
+								<Text fontSize={16} mt="0.2rem">
+									Price: {price}kr
+								</Text>
+							</Box>
+							<IconButton
+								icon={isProductLiked ? <GoHeartFill /> : <GoHeart />}
+								aria-label={isProductLiked ? "Unlike" : "Like"}
+								fontSize="1.5rem"
+								w="fit-content"
+								bg="none"
+								color="white"
+								_hover={{ bg: "none" }}
+								onClick={handleToggleLike}
+							/>
+						</Flex>
+						<Button
+							bg="none"
+							border="1px"
+							mt="0.2rem"
+							borderColor="white"
+							textColor="white"
+							_hover={{
+								bg: "whiteAlpha.200",
+								borderWidth: "2px",
+								boxShadow: "0 4px 8px rgba(255, 255, 255, 0.3)",
+							}}
+							onClick={handleOrderClick}
+						>
+							Order
+						</Button>
 					</Flex>
-				)}
-			</Link>
+				</Flex>
+			)}
 		</Flex>
 	);
 }
