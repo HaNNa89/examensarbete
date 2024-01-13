e0f3ce9bf92b0c96210c45338ca6cbd23e974b45
Tara skoglund <skoglundtara@gmail.com>
2024-01-13 10:54:49 +0000

    responsiv done

--- src/components/ProductCategories.tsx
+++ src/components/ProductCategories.tsx
@@ -1,4 +1,10 @@
-import { Box, Flex, HStack, Select } from '@chakra-ui/react';
+import {
+  Box,
+  Collapse,
+  Flex,
+  HStack,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 import { useState } from 'react';
 
 function ProductCategories({
@@ -8,54 +14,76 @@ function ProductCategories({
 }) {
   const categories = ['Brunch', 'Cake', 'Cookie'];
 
+  const [isFilterOpen, setIsFilterOpen] = useState(true);
   const [, setCurrentCategory] = useState<string | null>(null);
 
   const handleCategoryClick = (category: string) => {
     setCurrentCategory(category);
     onSelectCategory(category);
   };
+  const isLargeScreen = useBreakpointValue({ base: false, lg: true });
 
   return (
-    <Flex justifyContent="center">
-      <Box>
-        {/* Dropdown for smaller screens */}
-        <Select
-          display={{ base: 'block', lg: 'none' }}
-          onChange={(e) => handleCategoryClick(e.target.value)}
-        >
-          <option value="" disabled selected>
-            Select Category
-          </option>
-          {categories.map((category, index) => (
-            <option key={index} value={category}>
-              {category}
-            </option>
-          ))}
-        </Select>
+    <Flex justifyContent="center" mt={2}>
+      {isLargeScreen ? (
+        <Box>
+          <HStack>
+            {categories.map((category, index) => (
+              <Box
+                mt={6}
+                py={1}
+                px={6}
+                key={index}
+                onClick={() => handleCategoryClick(category)}
+                cursor="pointer"
+                fontSize={18}
+                border="1px"
+                _hover={{
+                  bg: 'whiteAlpha.200',
+                  borderWidth: '2px',
+                  boxShadow: '0 4px 8px rgba(255, 255, 255, 0.3)',
+                }}
+              >
+                {category}
+              </Box>
+            ))}
+          </HStack>
+        </Box>
+      ) : (
+        <Box>
+          <Box
+            mt={6}
+            py={1}
+            px={6}
+            fontSize={18}
+            border="1px"
+            onClick={() => setIsFilterOpen(!isFilterOpen)}
+            _hover={{
+              bg: 'whiteAlpha.200',
+              borderWidth: '2px',
+              boxShadow: '0 4px 8px rgba(255, 255, 255, 0.3)',
+            }}
+          >
+            Filter
+          </Box>
 
-        {/* Horizontal list for larger screens */}
-        <HStack display={{ base: 'none', lg: 'flex' }}>
-          {categories.map((category, index) => (
-            <Box
-              mt={6}
-              py={1}
-              px={6}
-              key={index}
-              onClick={() => handleCategoryClick(category)}
-              cursor="pointer"
-              fontSize={18}
-              border="1px"
-              _hover={{
-                bg: 'whiteAlpha.200',
-                borderWidth: '2px',
-                boxShadow: '0 4px 8px rgba(255, 255, 255, 0.3)',
-              }}
-            >
-              {category}
-            </Box>
-          ))}
-        </HStack>
-      </Box>
+          <Collapse in={isFilterOpen} animateOpacity>
+            <Flex direction="column" alignItems="center">
+              {categories.map((category, index) => (
+                <Box
+                  m={4}
+                  key={index}
+                  onClick={() => handleCategoryClick(category)}
+                  fontSize={18}
+                  borderBottom="1px"
+                >
+                  {category}
+                </Box>
+              ))}
+            </Flex>
+          </Collapse>
+        </Box>
+      )}
     </Flex>
   );
 }
