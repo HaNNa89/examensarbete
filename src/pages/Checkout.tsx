370ddcf2c5cf5b4e7341be90351370491c5a0092
HaNNa89 <petersson.h@hotmail.com>
2024-01-18 23:38:26 +0100

    remove console.log

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,9 +1,12 @@
 import { Center, Divider, Heading } from "@chakra-ui/react";
 import CustomerForm from "../components/CustomerForm";
 import ShoppingCartCheckout from "../components/ShoppingCartCheckout";
+import { useOrder } from "../hooks/useOrderContext";
 
 function Checkout() {
-	const handleOrderClick = () => {};
+	const { order } = useOrder();
+
+	console.log("Current order in Checkout:", order);
 	return (
 		<Center flexDir="column">
 			<Heading m="8">Checkout</Heading>
