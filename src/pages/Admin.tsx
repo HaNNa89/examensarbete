cbe7fd61fa86a2c5b2308309c34a9ba9d1801199
Tara skoglund <skoglundtara@gmail.com>
2024-01-22 19:40:28 +0100

    navigate added

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,5 +1,5 @@
 import { Button, Center, Flex, Grid, Heading, Text } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { MOCK_PRODUCTS, Product } from '../../data/mock';
 import AdminCard from '../components/AdminCard';
 
@@ -8,6 +8,7 @@ interface AdminProps {
 }
 
 function Admin({ product }: AdminProps) {
+  const navigate = useNavigate();
   if (!product && MOCK_PRODUCTS.length === 0) {
     return (
       <Center>
@@ -15,28 +16,32 @@ function Admin({ product }: AdminProps) {
       </Center>
     );
   }
+
+  const handleAddProductClick = () => {
+    navigate('/adminForm/new');
+  };
+
   return (
     <Center>
       <Flex direction="column" align="center" gap="6" m="6">
         <Heading>Admin!</Heading>
-        <Link to="adminForm/new">
-          <Button
-            mt={3}
-            py={1}
-            px={6}
-            fontSize={18}
-            border="1px"
-            bgColor="#1A1A1C"
-            textColor="white"
-            _hover={{
-              bg: 'whiteAlpha.200',
-              borderWidth: '2px',
-              boxShadow: '0 4px 8px rgba(255, 255, 255, 0.3)',
-            }}
-          >
-            Add Product
-          </Button>
-        </Link>
+        <Button
+          mt={3}
+          py={1}
+          px={6}
+          fontSize={18}
+          border="1px"
+          bgColor="#1A1A1C"
+          textColor="white"
+          _hover={{
+            bg: 'whiteAlpha.200',
+            borderWidth: '2px',
+            boxShadow: '0 4px 8px rgba(255, 255, 255, 0.3)',
+          }}
+          onClick={handleAddProductClick}
+        >
+          Add Product
+        </Button>
         <Center>
           <Grid
             templateColumns={{
